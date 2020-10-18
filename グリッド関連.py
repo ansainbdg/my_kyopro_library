54c01406df8127a70bb0c9a666c1ec86af85a6db
ansainbdg <tit1campus@hotmail.com>
2020-10-18 20:12:50 +0900

    グリッドグラフ関連を埋めるテンプレート

--- グリッド関連.py
+++ グリッド関連.py
@@ -1,4 +1,4 @@
-def main():
+def main():#グリッドグラフを隣接リストに置き換えるテンプレート
     h, w = map(int, input().split())
     s = [list(input()) for i in range(h)]
     edges=[[] for i in range(h*w)]
@@ -12,3 +12,13 @@ def main():
             if j != w-1:
                 if s[i][j] == s[i][j+1]:
                     pass
+
+def ume(grid, fill='#'):#gridの周辺をfillで埋める関数
+    result = []
+    result.append([fill]*(len(grid[0])+2))
+    for i in range(len(grid)):
+        gyou = [fill] + grid[i] + [fill]
+        result.append(gyou)
+
+    result.append([fill]*(len(grid[0])+2))
+    return result
