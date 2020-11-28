bd9749f1e3611c7db685b50fb59021c9188fb90c
ansainbdg <tit1campus@hotmail.com>
2020-11-28 20:43:47 +0900

    atcoderテンプレート

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
 import sys
 def input(): return sys.stdin.readline().rstrip()
 
+
 def main():
-    
-    
+    mod = 10**9+7
+    mod2 = 998244353
+
+
 if __name__ == '__main__':
     main()
