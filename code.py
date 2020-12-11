a737d6e5b7f3e1cd4d615bf35054cd90dfa24ad3
ansainbdg <tit1campus@hotmail.com>
2020-12-11 12:22:07 +0900

    コードテンプレート

--- code.py
+++ code.py
@@ -1,4 +1,12 @@
 import sys
+from collections import defaultdict,Counter,deque
+from itertools import permutations,combinations,product,combinations_with_replacement,groupby,accumulate
+#from math import sqrt,isqrt
+#from bisect import bisect_left,bisect_right
+#from functools import lru_cache,reduce
+#import heapq
+#import numpy as np
+#import networkx as nx
 def input(): return sys.stdin.readline().rstrip()
 
 
