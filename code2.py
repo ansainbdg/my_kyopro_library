f9cf0432c12bf02386bf091e89920f44c45cb460
ansainbdg <tit1campus@hotmail.com>
2021-01-06 00:48:54 +0900

    コードテンプレート

--- code2.py
+++ code2.py
@@ -1,6 +1,6 @@
 import sys
-from collections import defaultdict,Counter,deque
-from itertools import permutations,combinations,product,combinations_with_replacement,groupby,accumulate
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
 import operator
 #from math import sqrt,isqrt
 #from bisect import bisect_left,bisect_right
@@ -8,8 +8,10 @@ import operator
 #import heapq
 #import numpy as np
 #import networkx as nx
-#from numba import njit
+#from numba import njit, b1, i1, i4, i8, f8
+# numba例 @njit(i1(i4[:], i8[:, :]),catch=True) 引数i4配列、i8 2次元配列,戻り値i1
 def input(): return sys.stdin.readline().rstrip()
+def divceil(n, k): return 1+(n-1)//k  # n/kの切り上げを返す
 
 
 def main():
@@ -19,3 +21,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
