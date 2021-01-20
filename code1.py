5027822d70cd6a74ee00364407e17e32cbc29bfc
ansainbdg <tit1campus@hotmail.com>
2021-01-21 06:17:40 +0900

    コードテンプレート

--- code1.py
+++ code1.py
@@ -8,8 +8,8 @@ import operator
 #import heapq
 #import numpy as np
 #import networkx as nx
-#from numba import njit, b1, i1, i4, i8, f8
-# numba例 @njit(i1(i4[:], i8[:, :]),catch=True) 引数i4配列、i8 2次元配列,戻り値i1
+# from numba import njit, b1, i1, i4, i8, f8
+# numba例 @njit(i1(i4[:], i8[:, :]),cache=True) 引数i4配列、i8 2次元配列,戻り値i1
 def input(): return sys.stdin.readline().rstrip()
 def divceil(n, k): return 1+(n-1)//k  # n/kの切り上げを返す
 
@@ -17,7 +17,9 @@ def divceil(n, k): return 1+(n-1)//k  # n/kの切り上げを返す
 def main():
     mod = 10**9+7
     mod2 = 998244353
+    
 
 
 if __name__ == '__main__':
     main()
+
