837c82ece43c275e29db62995c782a577af2edf6
ansainbdg <tit1campus@hotmail.com>
2021-02-08 23:44:14 +0900

    テンプレ

--- code1.py
+++ code1.py
@@ -2,14 +2,18 @@ import sys
 from collections import defaultdict, Counter, deque
 from itertools import permutations, combinations, product, combinations_with_replacement, groupby, accumulate
 import operator
-#from math import sqrt,isqrt
+from math import sqrt, gcd, factorial
+#from math import isqrt, prod,comb  # python3.8用(notpypy)
 #from bisect import bisect_left,bisect_right
 #from functools import lru_cache,reduce
-#import heapq
+#from heapq import heappush,heappop,heapify,heappushpop,heapreplace
 #import numpy as np
 #import networkx as nx
-# from numba import njit, b1, i1, i4, i8, f8
-# numba例 @njit(i1(i4[:], i8[:, :]),cache=True) 引数i4配列、i8 2次元配列,戻り値i1
+#from networkx.utils import UnionFind
+#from numba import njit, b1, i1, i4, i8, f8
+#from scipy.sparse import csr_matrix
+#from scipy.sparse.csgraph import shortest_path, floyd_warshall, dijkstra, bellman_ford, johnson, NegativeCycleError
+#numba例 @njit(i1(i4[:], i8[:, :]),cache=True) 引数i4配列、i8 2次元配列,戻り値i1
 def input(): return sys.stdin.readline().rstrip()
 def divceil(n, k): return 1+(n-1)//k  # n/kの切り上げを返す
 
@@ -17,9 +21,9 @@ def divceil(n, k): return 1+(n-1)//k  # n/kの切り上げを返す
 def main():
     mod = 10**9+7
     mod2 = 998244353
-    
 
 
 if __name__ == '__main__':
     main()
+
 
