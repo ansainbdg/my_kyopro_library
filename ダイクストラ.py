732b05e7e21beb9f11dcede6f5f375f50346de9d
ansainbdg <tit1campus@hotmail.com>
2021-02-24 17:12:29 +0900

    ダイクストラ.py

--- ダイクストラ.py
+++ ダイクストラ.py
@@ -77,6 +77,44 @@ def main2():
         else:
             print(AA)
 
+# 枝刈りしてheapqに入れる値を制限ver2
+def main3():
+    v, e, r = map(int, input().split())
+
+    graph = [[] for _ in range(v)]
+
+    for i in range(e):
+        s, t, d = map(int, input().split())
+        graph[s].append((t, d))
+        #graph[t].append((s, d))
+
+    # 距離が確定→正,距離が未確定→負
+    mindist = [10**20] * v
+    hq = []
+    heappush(hq, (0, r))
+    while hq:
+        dist, node = heappop(hq)
+        """ゴールが1か所だけならこれで枝刈り
+        if noad==goal:
+            print(dist)
+            return
+        """
+        if mindist[node] < dist:  
+            continue
+        mindist[node] = dist
+        for i, nagasa in graph[node]:
+            if dist+nagasa >= mindist[i]:  # 距離が暫定的にhqに入っているより大きいならhqに入れない
+                continue
+            heappush(hq, (dist+nagasa, i))
+            mindist[i] = dist+nagasa  # 距離が暫定距離を更新する
+    
+    #print(-1) #ゴールが1か所だけならこれを使う
+    for AA in mindist:
+        if AA == 10**20:
+            print('INF')
+        else:
+            print(AA)
+
 
 if __name__ == '__main__':
-    main()
+    main3()
