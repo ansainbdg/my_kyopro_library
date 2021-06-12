269670122d07557269af1903249ea1e0b2dfb6d5
ansainbdg <tit1campus@hotmail.com>
2021-06-13 05:35:41 +0900

    掃き出し法

--- xor掃き出し.py
+++ xor掃き出し.py
@@ -29,3 +29,20 @@ def hakidashi_hantei(ans, K):
         if highest_bit(aa) & K:
             K ^= aa
     return K == 0
+
+def hakidashi2(A):
+    x,y=A.shape
+    ans = []
+    for i in range(y):
+        if A[:,i].max() != 0:
+            A=A[np.argsort(A[:,i])[::-1]]
+            ans.append(np.copy(A[0]))
+            #print(A)
+            A[np.where(A[:,i]>0)[0]]^=A[0]
+    return np.stack(ans)
+ 
+def hakidashi_hantei2(ans, K):
+    for aa in ans:
+        if K[np.where(aa==1)[0][0]]!=0:
+            K ^= aa
+    return np.all(K == np.zeros_like(K))
