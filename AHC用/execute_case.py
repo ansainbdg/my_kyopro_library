847627a42adfa6f66837c2d85a3d150105d7d208
ansain <tit1campus@hotmail.com>
2023-06-25 05:14:16 +0900

    修正

--- AHC用/execute_case.py
+++ AHC用/execute_case.py
@@ -6,7 +6,10 @@ def execute_case(seed,TL=None,file="code1.py"):
     output_file_path = f'out/{seed:04}.txt'
     with open(input_file_path) as fin:
         with open(output_file_path, 'w') as fout:
-            subprocess.run(['python',file], stdin=fin, stdout=fout, timeout=TL)
+            proc = subprocess.run(['python',file], stdin=fin, stdout=fout, stderr=subprocess.PIPE,timeout=TL)
+            print(proc.stderr.decode('utf8').split('\n'))
+            if "Err" in proc.stderr.decode('utf8'):
+                raise
             pipefile = f'pipeline/pipefile_{seed:04}'
             with pipes.Template().open(pipefile, 'w') as p:
                 subprocess.run(['cargo', "run", "--release","--bin", "vis", input_file_path, output_file_path], stdout=p, timeout=TL)
@@ -17,10 +20,13 @@ def execute_case(seed,TL=None,file="code1.py"):
 def execute_case_interactive(seed,TL=None,file="code1.py"):
     input_file_path = f'in/{seed:04}.txt'
     output_file_path = f'out/{seed:04}.txt'
-    subprocess.run(f'cargo run --release --bin tester python {file} < {input_file_path} > {output_file_path}',shell=True)
+    proc = subprocess.run(f'cargo run --release --bin tester python {file} < {input_file_path} > {output_file_path}',stderr=subprocess.PIPE,shell=True,timeout=TL)
+    print(proc.stderr.decode('utf8').split('\n'))
+    if "Err" in proc.stderr.decode('utf8'):
+        raise
     pipefile = f'pipeline/pipefile_{seed:04}'
     with pipes.Template().open(pipefile, 'w') as p:
-        subprocess.run(['cargo', "run", "--release","--bin", "vis", input_file_path, output_file_path], stdout=p, timeout=TL)
+        subprocess.run(['cargo', "run", "--release","--bin", "vis", input_file_path, output_file_path], stderr=p, timeout=TL)
     output = open(pipefile).read()
     assert output
     return seed, output
